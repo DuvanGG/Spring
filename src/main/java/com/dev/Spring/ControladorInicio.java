8d0d3985a5a021141debbea41fb7e3f7e58337e1
DuvanGG <duvanga@hotmail.es>
2024-02-12 20:50:33 -0500

    inicio con thymeleaf

--- src/main/java/com/dev/Spring/ControladorInicio.java
+++ src/main/java/com/dev/Spring/ControladorInicio.java
@@ -1,18 +1,17 @@
 package com.dev.Spring;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
 	
 	@GetMapping("/")
 	public String inicio() {
-		log.info("Ejecutando el controlador rest");
-		return "Hola mundo con spring";
+		log.info("Ejecutando el controlador Spring MVC");
+		return "index";
 	}
 
 }
