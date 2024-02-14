7209a939350c8fe5015064f39d9db187fe15b24e
DuvanGG <duvanga@hotmail.es>
2024-02-13 20:25:13 -0500

    Se crea la clase de sominio de Persona

--- src/main/java/com/dev/Spring/ControladorInicio.java
+++ src/main/java/com/dev/Spring/ControladorInicio.java
@@ -1,5 +1,8 @@
 package com.dev.Spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,9 +21,17 @@ public class ControladorInicio {
 	@GetMapping("/")
 	public String inicio(Model model) {
 		var mensaje = "Hola mundo con thymeleaf";
+		var persona =  new Persona();
+		var persona2 =  new Persona();
+		persona.setNombre("Dev");
+		persona.setApellido("DevA");
+		persona.setEmail("dev@DevA.com");
+		persona.setTelefono("1233321");
+		
 		log.info("Ejecutando el controlador Spring MVC");
 		model.addAttribute("mensaje", mensaje);
 		model.addAttribute("saludo", saludo);
+		model.addAttribute("persona", persona);
 		return "index";
 	}
 
